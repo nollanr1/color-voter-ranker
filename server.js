525e35b4a1c5f4ea9e5cd8922135c6dae2a870aa
Robert Nollan <nollanr1@spu.edu>
2022-07-24 14:37:15 -0700

    Moving things around and reworking them to more closely match Leon's example. It's not voodoo programming if I actually understand it... I just am unable to do it without a reference document. Baby steps. Next up: the EJS

--- server.js
+++ server.js
@@ -1,28 +1,65 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const { restart } = require('nodemon');
+const { response, request } = require('express');
 require('dotenv').config();
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
 const PORT = 3000;
-const dbuser = process.env.DBUSER;
-const passphrase = process.env.DBPASSPHRASE;
-const connectionString = `mongodb+srv://${dbuser}:${passphrase}@colors-and-votes.ioosw.mongodb.net/?retryWrites=true&w=majority`
-
-app.use(bodyParser.urlencoded({ extended: true}));
+const connectionString = process.env.DBSTRING;
 
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
 	.then(client => {
 		console.log('Connected to Database')
+		const db = client.db('colors-and-votes');
 	})
 	.catch(error => console.error(error));
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
 app.get('/', (req, res) => {
-	res.sendFile(`${__dirname}/index.html`);
+	db.collection('colors').find().sort({votes: -1}).toArray()
+	.then(data => {
+		response.render('index.ejs', {info: data}) //TODO: Actually configure an EJS-based index page...
+	})
+	.catch(error => console.error(error));
 })
 
 app.post('/newcolor', (req, res) => {
 	console.log(req.body);
-	res.sendFile(`${__dirname}/index.html`); //TODO: Redirect. Or don't move at all...
+	db.collection('colors').insertOne({color: request.body.color, votes: 1})
+	.then(result => {
+		console.log(`Color ${request.body.color} added`);
+		response.redirect('/');
+	})
+	.catch(error => console.error(error));
+})
+
+app.put('/vote', (req, res) => {
+	db.collection(colors).updateOne({color: request.body.color, votes: request.body.votes},{
+		$set: {
+			votes:request.body.votes + 1
+		}
+	},{
+		sort: {_id: -1},
+		upsert: true
+	})
+	.then(result => {
+		console.log(`Added one vote for color ${request.body.color}, new total is ${request.body.votes + 1}`);
+		response.json('Vote tallied');
+	})
+	.catch(error => console.error(error));
+})
+
+app.delete('/deletecolor', (request, response) => {
+	db.collection('colors').deleteOne({color: request.body.color})
+	.then(result => {
+		console.log(`Color ${request.body.color} has been annihilated`);
+		response.json('Color deleted');
+	})
+	.catch(error => console.error(error));
 })
 
 app.listen(process.env.PORT || PORT, () => {
